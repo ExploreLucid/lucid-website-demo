dbbead7e3da03aa7609127f242ce14fce504afed
mding5692 <m.ding5692@gmail.com>
2016-08-24 22:50:15 -0400

    Finished Desktop version webpage

---
+++ js/firebase.js
@@ -0,0 +1,8 @@
+          // Initialize Firebase
+          var config = {
+            apiKey: "AIzaSyBnKeAxilDLlos8XTMWKH4fv0O203Y7_2s",
+            authDomain: "lucid-website.firebaseapp.com",
+            databaseURL: "https://lucid-website.firebaseio.com",
+            storageBucket: "lucid-website.appspot.com",
+          };
+          firebase.initializeApp(config);
