8181b32489b17c5907fa301e6761a26d52d95dfe
Yucen Zhang <yucen.zhang0205@gmail.com>
2017-04-05 21:53:01 -0400

    Scroll down animation and contact section done

--- js/new-age.js
+++ js/new-age.js
@@ -1,31 +1,25 @@
 (function($) {
     "use strict"; // Start of use strict
+    
+    // Handles scrolling down after clicking on page-down arrow button
+    $(".page-scroll").on('click', function(e) {
+       // prevent default anchor click behavior
+       e.preventDefault();
 
-    // jQuery for page scrolling feature - requires jQuery Easing plugin
-    $(document).on('click', 'a.page-scroll', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: ($($anchor.attr('href')).offset().top - 50)
-        }, 1250, 'easeInOutExpo');
-        event.preventDefault();
+       // store hash
+       var hash = this.hash;
+
+       // animate
+       $('body').animate({
+           scrollTop: $(hash).offset().top
+         }, 1000, function(){
+
+           // when done, add hash to url
+           // (default click behaviour)
+           window.location.hash = hash;
+         });
+
     });
-
-    // Highlight the top nav as scrolling occurs
-    $('body').scrollspy({
-        target: '.navbar-fixed-top',
-        offset: 100
-    });
-
-    // Closes the Responsive Menu on Menu Item Click
-    $('.navbar-collapse ul li a').click(function() {
-        $('.navbar-toggle:visible').click();
-    });
-
-    // Offset for Main Navigation
-    $('#mainNav').affix({
-        offset: {
-            top: 50
-        }
-    })
 
 })(jQuery); // End of use strict
+
